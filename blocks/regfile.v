9c61f5c1c2c8e2b5dd7bf10fa3fd88994b88b4fd
samentow <verschuer00@gmail.com>
2022-01-04 15:57:38 -0300

    register file

--- blocks/regfile.v
+++ blocks/regfile.v
@@ -1,30 +1,25 @@
-module regfile(rs1, rs2, rd, write_val, write_enable, clk, rs1_out, rs2_out);
-	input write_enable, clk;
-	input [4:0] rs1, rs2, rd;
+module regfile(clk, addr1, addr2, addrWrite, write, dataWrite, rs1, rs2);
+	input clk, write;
+	input [31:0] dataWrite;
+	input [4:0] addr1, addr2, addrWrite;
+	output reg[31:0] rs1, rs2;
 
-	input [31:0] write_val;
-	reg [31:0] registers[31:0];
-	
-	output [31:0] rs1_out, rs2_out;
+	reg[31:0] registers[31:0]; // 32-bit registers x 32
 
 	integer i;
-	
 	initial begin
 		for (i = 0; i < 32; i = i + 1) begin
-			registers[i] = 0;
+			registers[i] <= 0;
 		end
 	end
 
-	assign rs1_out = registers[rs1] < 32 && registers[rs1] >= 0 ? registers[rs1] : rs1_out;
-	assign rs2_out = registers[rs2] < 32 && registers[rs2] >= 0 ? registers[rs2] : rs2_out;
-
 	always @ (posedge clk) begin
-		case (write_enable) 
-			0: begin end
-			1: begin
-				registers[rd] = write_val;	
-			end
-		endcase
+		if (write && addrWrite != 5'b00000)
+			registers[addrWrite] <= dataWrite;
+		rs1 <= registers[addr1];
+		rs2 <= registers[addr2];
 	end
+
+
 
 endmodule
--- blocks/tb_regfile.v
+++ blocks/tb_regfile.v
@@ -1,23 +1,22 @@
 `include "regfile.v"
 module regfile_tb;
-	reg clk, en_w;
-	reg[4:0] rs1, rs2, rd;
-	reg[31:0] val_w;
-	wire[31:0] rs1_out, rs2_out;
+	reg clk = 0;
+	reg[4:0] addr1, addr2, addrWrite;
+	reg write = 0;
+	reg[31:0] dataWrite;
+	wire[31:0] rs1, rs2;
 
 	initial begin
-		$monitor("rs1 = %d\n rs2 = %d\n rd = %d\n write_enable = %d\n write_value = %d\n rs1_out = %d\n rs2_out = %d\n\n",
-		rs1, rs2, rd, en_w, val_w, rs1_out, rs2_out);
-		#5 rs1 =0; rs2=0; rd=0; val_w=0; en_w=0;
-		#10 rs1 =1; rs2=0; rd=1; val_w=24; en_w=1;
-		#10;
-		#5 rs1=1; rs2=0; rd=0; val_w=0; en_w=0;
+		$monitor("%d", rs1);
+		#5 addr1 = 4;
+		#20
+		#5 addrWrite = 4; dataWrite = 32; write =1;
+		#20 write = 0;
 	end
 
 	always begin
 		#5 clk = !clk;
 	end
 
-	regfile regfile_t(rs1, rs2, rd, val_w, en_w, clk, rs1_out, rs2_out);
-	
+	regfile rf(clk, addr1, addr2, addrWrite, write, dataWrite, rs1, rs2);
 endmodule
