c9f834ee700c2bf16cd5008afd169a1a58f2cfdd
sarmentow <pedrolins1309@gmail.com>
2022-01-15 17:16:32 -0300

    Added branches

--- blocks/core.v
+++ blocks/core.v
@@ -9,12 +9,12 @@ module core(clk);
 	// I need better names
 	wire [31:0] alu_out, rs1, rs2_or_imm, rs2, regfile_data; 
 	wire [31:0] dmem_out, dmem_address_calc, dmem_sel_data_out, dmem_data;
-	wire [31:0] pc_next, pc_out, ins;
+	wire [31:0] pc_next, pc_out, ins, branch_addr;
 	wire [31:0] jump_sel, jal_address_calc, jalr_address_calc, jalr_address_calc_last_bit_not_set;
 
 	wire regfile_write, dmem_write, pc_write;
 
-	wire should_jump;
+	wire should_jump, branch_or_jump, branch_condition_is_true;
 
 	// Write logic is temporary
 	assign regfile_write = ins[6:0] == 7'b0110011 ? 1'b1 : 
@@ -40,7 +40,19 @@ module core(clk);
 	assign jalr_address_calc = {jalr_address_calc_last_bit_not_set[31:1], 1'b0};
 	assign should_jump = (ins[6:0] == 7'b1101111 || ins[6:0] == 7'b1100111) ? 1 : 0;
 	assign jump_sel =  should_jump && ins[3] ? jal_address_calc : jalr_address_calc;
-	assign pc_next = should_jump ? jump_sel : pc_out + 4;
+	assign branch_addr = {ins[31],ins[7],ins[30:25],ins[11:8]} + pc_out;
+	// opcode is branch and condition passes?
+	assign pc_next = should_jump ? jump_sel : 
+	                 branch_condition_is_true ? branch_addr : pc_out + 4;
+
+
+	assign branch_condition_is_true = (ins[6:0] == 7'b1100011) && // is branch
+		                              ((ins[14:12] == 3'b000 && rs1 == rs2) ||  // is equal in beq
+									  (ins[14:12] == 3'b001 && rs1 != rs2) || // is not equal in bne
+									  (ins[14:12] == 3'b100 && rs1 < rs2) || // ...
+									  (ins[14:12] == 3'b101 && rs1 > rs2) ||
+									  (ins[14:12] == 3'b110 && $unsigned(rs1) < $unsigned(rs2)) ||
+									  (ins[14:12] == 3'b111 && $unsigned(rs1) > $unsigned(rs2)));
 
 	
 	pc program_counter(pc_next, clk, pc_write, pc_out);
